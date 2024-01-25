1f51f0d90c7b63adddfab9e104150f5b8eff555b
Rithik Suthan S <rithiksuthan.smartail@gmail.com>
2024-01-25 15:18:26 +0530

    added some code for board members

---
+++ src/Components/Board/Board.js
@@ -0,0 +1,14 @@
+import React from "react";
+class Board extends React.Component
+{
+    render()
+    {
+        return(
+            <body>
+            <h1>Board Members</h1>
+            </body>
+        );
+    }
+}
+
+export default Board;
