0730ee95ab46be04e84fc312ee051c02cab5ff48
Rithik Suthan S <rithiksuthan.smartail@gmail.com>
2024-01-26 21:26:31 +0530

    developer page without fixing the css

--- src/Components/Developers/Developers.js
+++ src/Components/Developers/Developers.js
@@ -8,13 +8,34 @@ import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import PhoneCallbackIcon from '@mui/icons-material/PhoneCallback';
+import rithik from "../../Assets/rithik.png";
+import abhinivesh from "../../Assets/abhinivesh.JPG";
+import susma from "../../Assets/susma.jpg";
 class Developers extends React.Component
 {
-    icons = [
-        { icon: <LinkedInIcon sx={{color:"#0077B5"}} />,link:"https://www.linkedin.com/in/mohan-prasaath-2015651b8/" },
-        { icon: <EmailOutlinedIcon sx={{color:"GrayText"}}/>, link: "mailto:mohanprasaath269@gmail.com" },
-        { icon: <PhoneCallbackIcon sx={{color:"green"}}/>, link: "tel:+91 9842258390" },
-        { icon: <InstagramIcon sx={{color:"#833AB4"}}/>, link: "https://www.instagram.com/mohan._.17" },
+    iconsAbhinivesh = [
+        { icon: <LinkedInIcon sx={{color:"#0077B5"}} />,link:"https://www.linkedin.com/in/abhinivesh0305/" },
+        { icon: <EmailOutlinedIcon sx={{color:"GrayText"}}/>, link: "mailto:abhinivi67@gmail.com" },
+        { icon: <PhoneCallbackIcon sx={{color:"green"}}/>, link: "tel:+91 9345884174" },
+        { icon: <InstagramIcon sx={{color:"#833AB4"}}/>, link: "https://www.instagram.com/sound_of_keys/" },
+      ];
+      iconsRithik = [
+        { icon: <LinkedInIcon sx={{color:"#0077B5"}} />,link:"https://www.linkedin.com/in/rithiksuthan/" },
+        { icon: <EmailOutlinedIcon sx={{color:"GrayText"}}/>, link: "rithiksuthan123@gmail.com" },
+        { icon: <PhoneCallbackIcon sx={{color:"green"}}/>, link: "tel:+91 6383704021" },
+        { icon: <InstagramIcon sx={{color:"#833AB4"}}/>, link: "https://www.instagram.com/rithik_suthan/" },
+      ];
+      iconsJanumathi = [
+        { icon: <LinkedInIcon sx={{color:"#0077B5"}} />,link:"https://www.linkedin.com/in/janumathi-g-p/" },
+        { icon: <EmailOutlinedIcon sx={{color:"GrayText"}}/>, link: "mailto:janusivan05@gmail.com" },
+        { icon: <PhoneCallbackIcon sx={{color:"green"}}/>, link: "tel:+91 9360638869" },
+        { icon: <InstagramIcon sx={{ color: "#833AB4", cursor: 'not-allowed' }} onClick={() => null} />, link: null },
+      ];
+      iconsSusma = [
+        { icon: <LinkedInIcon sx={{color:"#0077B5"}} />,link:"https://www.linkedin.com/in/susmav/" },
+        { icon: <EmailOutlinedIcon sx={{color:"GrayText"}}/>, link: "mailto:susmav1012@gmail.com" },
+        { icon: <PhoneCallbackIcon sx={{color:"green"}}/>, link: "tel:+91 6385480548" },
+        { icon: <InstagramIcon sx={{color:"#833AB4"}}/>, link: "https://www.instagram.com/susmavijayakumar/" },
       ];
     render()
     {
@@ -30,41 +51,25 @@ class Developers extends React.Component
             <Grid container spacing={2} justifyContent="center" alignItems="center">
               <Grid item xs={12} md={6}>
                 <img
-                  src={mohanImage}
+                  src={abhinivesh}
                   style={{
                     height: "100%",
                     width: "100%",
                     objectFit: "cover",
                   }}
-                  alt="Mohan"
+                  alt="Abhinivesh"
                 />
               </Grid>
               <Grid item xs={12} md={6} sx={{ display: "flex", flexDirection: "column", justifyContent: "center" }}>
                 <CardContent>
                   <Typography variant="h4" sx={{ fontSize: { xs: "20px", md: "28px" }, textAlign: { xs: "center", md: "left" }, whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }} fontWeight="700">
-                    MOHAN PRASAATH S
+                  Abhinivesh S
                   </Typography>
                   <Typography mb={4} color="#6A6A6A" textAlign={{ xs: "center", md: "left" }}>
-                    Msc Software Systems - 4th Year
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    component="p"
-                    mt={1}
-                    letterSpacing={0.6}
-                    lineHeight={1.8}
-                    sx={{ textIndent: { xs: "0", md: "20px" }, textAlign: { xs: "center", md: "left" } }}
-                  >
-                    As someone who loves to explore and learn, I'm always searching
-                    for new opportunities to challenge myself and grow. I thrive in
-                    dynamic and fast-paced environments, and I'm not afraid to take
-                    risks. I'm passionate about making a meaningful impact in my
-                    field, and I'm excited to connect with like-minded professionals
-                    to discover new possibilities and create something extraordinary
-                    together.
+                  M.Sc. Decision and Computing Sciences - 4th Year
                   </Typography>
                   <Box mt={2}>
-                    {this.icons.map(({ icon, link }) => (
+                    {this.iconsAbhinivesh.map(({ icon, link }) => (
                       <IconButton key={link} href={link} target="_blank">
                         {icon}
                       </IconButton>
@@ -83,48 +88,29 @@ class Developers extends React.Component
       <Grid item xs={12} md={6} sx={{ display: "flex", flexDirection: "column", justifyContent: "center" }}>
         <CardContent>
           <Typography variant="h4" sx={{ fontSize: { xs: "20px", md: "28px" }, textAlign: { xs: "center", md: "left" }, whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }} fontWeight="700">
-            ANOTHER DEVELOPER
+          Rithik Suthan S
           </Typography>
           <Typography mb={4} color="#6A6A6A" textAlign={{ xs: "center", md: "left" }}>
-            Role - Experience
+          M.Sc. Software Systems - 4th Year
           </Typography>
-          <Typography
-            variant="body2"
-            component="p"
-            mt={1}
-            letterSpacing={0.6}
-            lineHeight={1.8}
-            sx={{ textIndent: { xs: "0", md: "20px" }, textAlign: { xs: "center", md: "left" } }}
-          >
-            Description about the another developer goes here. Customize it based on your needs.
-          </Typography>
-          <Box mt={2}>
-            {/* Social media icons go here */}
-            {/* Example: */}
-            <IconButton href="/" target="_blank">
-              <EmailOutlinedIcon />
-            </IconButton>
-            <IconButton href="/" target="_blank">
-              <LinkedInIcon />
-            </IconButton>
-            <IconButton href="/" target="_blank">
-              <InstagramIcon />
-            </IconButton>
-            <IconButton href="/" target="_blank">
-              <PhoneCallbackIcon />
-            </IconButton>
-          </Box>
+                  <Box mt={2}>
+                    {this.iconsRithik.map(({ icon, link }) => (
+                      <IconButton key={link} href={link} target="_blank">
+                        {icon}
+                      </IconButton>
+                    ))}
+                  </Box>
         </CardContent>
       </Grid>
       <Grid item xs={12} md={6}>
         <img
-          src={mohanImage}
+          src={rithik}
           style={{
             height: "100%",
             width: "100%",
             objectFit: "cover",
           }}
-          alt="Another Developer"
+          alt="Rithik"
         />
       </Grid>
     </Grid>
@@ -142,36 +128,20 @@ class Developers extends React.Component
                     width: "100%",
                     objectFit: "cover",
                   }}
-                  alt="Mohan"
+                  alt="Janumathi"
                 />
               </Grid>
               <Grid item xs={12} md={6} sx={{ display: "flex", flexDirection: "column", justifyContent: "center" }}>
                 <CardContent>
                   <Typography variant="h4" sx={{ fontSize: { xs: "20px", md: "28px" }, textAlign: { xs: "center", md: "left" }, whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }} fontWeight="700">
-                    MOHAN PRASAATH S
+                  Janumathi G P 
                   </Typography>
                   <Typography mb={4} color="#6A6A6A" textAlign={{ xs: "center", md: "left" }}>
-                    Msc Software Systems - 4th Year
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    component="p"
-                    mt={1}
-                    letterSpacing={0.6}
-                    lineHeight={1.8}
-                    sx={{ textIndent: { xs: "0", md: "20px" }, textAlign: { xs: "center", md: "left" } }}
-                  >
-                    As someone who loves to explore and learn, I'm always searching
-                    for new opportunities to challenge myself and grow. I thrive in
-                    dynamic and fast-paced environments, and I'm not afraid to take
-                    risks. I'm passionate about making a meaningful impact in my
-                    field, and I'm excited to connect with like-minded professionals
-                    to discover new possibilities and create something extraordinary
-                    together.
+                  M.Sc. Software Systems - 4th Year
                   </Typography>
                   <Box mt={2}>
-                    {this.icons.map(({ icon, link }) => (
-                      <IconButton key={link} href={link} target="_blank">
+                    {this.iconsJanumathi.map(({ icon, link }) => (
+                      <IconButton key={link} href={link} target="_blank" onClick={link ? null : () => null}>
                         {icon}
                       </IconButton>
                     ))}
@@ -189,48 +159,29 @@ class Developers extends React.Component
       <Grid item xs={12} md={6} sx={{ display: "flex", flexDirection: "column", justifyContent: "center" }}>
         <CardContent>
           <Typography variant="h4" sx={{ fontSize: { xs: "20px", md: "28px" }, textAlign: { xs: "center", md: "left" }, whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }} fontWeight="700">
-            ANOTHER DEVELOPER
+            Susma V
           </Typography>
           <Typography mb={4} color="#6A6A6A" textAlign={{ xs: "center", md: "left" }}>
-            Role - Experience
+          M.Sc. Software Systems - 4th Year
           </Typography>
-          <Typography
-            variant="body2"
-            component="p"
-            mt={1}
-            letterSpacing={0.6}
-            lineHeight={1.8}
-            sx={{ textIndent: { xs: "0", md: "20px" }, textAlign: { xs: "center", md: "left" } }}
-          >
-            Description about the another developer goes here. Customize it based on your needs.
-          </Typography>
-          <Box mt={2}>
-            {/* Social media icons go here */}
-            {/* Example: */}
-            <IconButton href="/" target="_blank">
-              <EmailOutlinedIcon />
-            </IconButton>
-            <IconButton href="/" target="_blank">
-              <LinkedInIcon />
-            </IconButton>
-            <IconButton href="/" target="_blank">
-              <InstagramIcon />
-            </IconButton>
-            <IconButton href="/" target="_blank">
-              <PhoneCallbackIcon />
-            </IconButton>
-          </Box>
+                  <Box mt={2}>
+                    {this.iconsSusma.map(({ icon, link }) => (
+                      <IconButton key={link} href={link} target="_blank">
+                        {icon}
+                      </IconButton>
+                    ))}
+                  </Box>
         </CardContent>
       </Grid>
       <Grid item xs={12} md={6}>
         <img
-          src={mohanImage}
+          src={susma}
           style={{
             height: "100%",
             width: "100%",
             objectFit: "cover",
           }}
-          alt="Another Developer"
+          alt="Susma"
         />
       </Grid>
     </Grid>
