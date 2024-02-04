e07816b9f07b8a046434f0f89384df23847bc4ad
Janumathi <janusivan05@gmail.com>
2024-02-04 12:15:08 +0530

    about button

--- src/Components/aboutMelinia.js
+++ src/Components/aboutMelinia.js
@@ -21,7 +21,8 @@ function AboutMelinia() {
    <span className="aboutFont">ABOUT MELINIA</span> 
    <p className="rajdhaniFont" style={{textAlign:'justify', marginTop:'20px'}}>The Department of Computing organizes a National level inter-college technical symposium "Melinia". It is a celebration of technology, creativity and innovation, attracting students from around the state. It provides a national platform for the students to showcase their talents and motivates through workshops conducted by industrial experts. It also provides an opportunity for the companies to network and engage with the next generation of technology leaders.</p>
    <Divider className="horizontalDivider" style={{ padding: '0', margin: '0', marginTop: '10px', borderColor: 'black' }} />
-   <Button className="buttonFont" style={{ backgroundColor: '#61016e' }} variant="contained" >About <FaArrowRight style={{ marginLeft: '3px' }} /></Button>   </div>
+   {/* <Button className="buttonFont" style={{ backgroundColor: '#61016e' }} variant="contained" >About <FaArrowRight style={{ marginLeft: '3px' }} /></Button>   */}
+    </div>
  </div>
   );
 }
